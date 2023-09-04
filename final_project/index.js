86f805ed3ca66a65200cb7f2bafaa58191eb2419
Luiz Henrique Ardezzoni <143741357+lardezzoni@users.noreply.github.com>
2023-09-04 17:04:31 -0300

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -6,12 +6,71 @@ const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
+let users = []
+
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
+  
 app.use(express.json());
+
+app.post("/register", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User registred."});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    if(!username || !password){
+        return res.status(404).json({message: "Enter a valid username or password."});
+    }
+    return res.status(404).json({message: "Unable to register user."});
+  });
+  
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if(req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "User not logged in"})
+     }
 });
  
 const PORT =5000;
